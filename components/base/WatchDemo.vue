7b5d7c27af07281d7b477befb64410cd9e0a0dc8
stormforge942 <ace95322@gmail.com>
2024-10-01 14:55:16 -0300

    feat: add watchdemo modal

--- components/base/WatchDemo.vue
+++ components/base/WatchDemo.vue
@@ -1,15 +1,26 @@
+<script setup lang="ts">
+  import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import Modal from './Modal.vue';
+  const isOpen = ref(false);
+
+  const toggle = (val: boolean) => {
+    isOpen.value = val;
+  }
+</script>
+
 <template>
-    <button class="flex items-center border-none w-[190px] h-[40px] hover:scale-[1.01]">
-      <span class="flex justify-center items-center w-[calc(100%-40px)] h-full bg-white text-[#021d35] text-md font-semibold">
-        {{ $t('menu.watch_the_demo') }}
-      </span>
-      <span class="flex justify-center items-center w-[40px] h-full bg-secondary text-black">
-        <FontAwesome :icon="faCaretRight" class="transform scale-150" />
-      </span>
-    </button>
-  </template>
-  
-  <script setup lang="ts">
-  import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
-  </script>
+  <button
+  @click="isOpen = true" 
+  class="flex items-center border-none w-[190px] h-[40px] hover:scale-[1.01]">
+    <span class="flex justify-center items-center w-[calc(100%-40px)] h-full bg-white text-[#021d35] text-md font-semibold">
+      {{ $t('menu.watch_the_demo') }}
+    </span>
+    <span class="flex justify-center items-center w-[40px] h-full bg-secondary text-black">
+      <FontAwesome :icon="faCaretRight" class="transform scale-150" />
+    </span>
+  </button>
+  <Modal :bodyClass="'w-4/5 h-4/5'" :isOpen="isOpen" @toggle="toggle">
+
+  </Modal>
+</template>
   
