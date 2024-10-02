7a05245a9db7ab092d1ae336f69f6e6fd77cad24
stormforge942 <ace95322@gmail.com>
2024-10-02 10:49:09 -0300

    feat: add terms of service & privacy policy page

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+
+useHead({
+  title: 'Privacy Policy',
+})
+</script>
+
+<template>
+  <NuxtLayout name="main">
+    <template #header>{{ $t('auth.register.form.labels.link_privacy_policy') }}</template>
+
+  </NuxtLayout>
+</template>
---
+++ pages/terms-of-service.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+
+useHead({
+  title: 'Terms of Service',
+})
+</script>
+
+<template>
+  <NuxtLayout name="main">
+    <template #header>{{ $t('auth.register.form.labels.link_tos') }}</template>
+
+  </NuxtLayout>
+</template>
